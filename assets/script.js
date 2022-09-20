907adc97ebeeb95805030598d38795a48d7e2cf6
Duran11 <antonio11duran@gmail.com>
2022-09-20 14:04:19 -0600

    added fetch function

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+var submitButton = document.getElementById("submit-button");
+var weatherContainer = document.querySelector("weather-container");
+var cityInput = document.getElementById("city-input");
+
+function getApi(event) {
+    event.preventDefault();
+
+    var city = cityInput.value.trim();
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=fc16f87174dc7bfe353eec13a82879dc';
+
+    fetch(apiUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
+
+submitButton.addEventListener("click", getApi);
