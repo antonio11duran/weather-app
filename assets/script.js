d35a5aca2908a00d3ad0c7b87dd97b4cd8541919
Duran11 <antonio11duran@gmail.com>
2022-09-22 10:30:12 -0600

    added some console logs to bug fix, still need to identify why html elements are not beign generated

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,81 @@
 var submitButton = document.getElementById("submit-button");
-var weatherContainer = document.querySelector("weather-container");
+var weatherContainer = document.getElementById("weather-container");
 var cityInput = document.getElementById("city-input");
+var historyList = document.querySelector("btn-group-vertical");
+var todayData = document.getElementById("today-data");
+var forecastData = document.getElementById("forecast-data");
 
-function getApi(event) {
+var formSubmitHandler = function (event) {
     event.preventDefault();
 
     var city = cityInput.value.trim();
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=fc16f87174dc7bfe353eec13a82879dc';
+
+    if (city) {
+        getApi(city);
+
+        weatherContainer.textContent = '';
+        cityInput.value = '';
+    } else {
+        alert("Please enter a city name.")
+    }
+}
+
+function getApi(city) {
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=fc16f87174dc7bfe353eec13a82879dc&units=imperial';
 
     fetch(apiUrl)
         .then(function (response) {
-            return response.json();
+            if (response.ok) {
+                return response.json().then(function (data) {
+                    console.log(data);
+                    displayWeather(data);
+                });
+            } else {
+                alert("Error: " + response.statusText);
+            }
         })
-        .then(function (data) {
-            console.log(data);
+        .catch(function (error) {
+            alert("Unable to connect to weather API.");
         })
+};
+
+var displayWeather = function(info) {
+    console.log(info);
+    console.log(info.city.name);
+    console.log(info.list[0].main.temp);
+    console.log((info.list).length);
+
+    var listArray = info.list;
+
+    console.log(listArray);
+
+    var cityName = info.city.name;
+    
+    for (var i = 0; i < listArray.length; i++) {
+        var cityDate = moment(listArray[i].dt).format("l");
+        var icon = listArray[i].weather[0].icon
+        var temperature = listArray[i].main.temp;
+        var windSpeed = listArray[i].wind.speed;
+        var humidity = listArray[i].main.humidity;
+
+        if (i === 0) {
+            var todayTitle = document.createElement('h2');
+            todayTitle.textContent = cityName + " (" + cityDate + ") " + icon;
+            todayData.appendChild(todayTitle);
+
+            var todayTemp = document.createElement('span');
+            todayTemp.textContent = "Temp: " + temperature + "°F";
+            todayTitle.appendChild(todayTemp);
+
+            var todayWind = document.createElement('span');
+            todayWind.textContent = "Wind: " + windSpeed + " MPH";
+            todayTemp.appendChild(todayWind);
+
+            var todayHumid = document.createElement('span');
+            todayHumid.textContent = "Humidity: " + humidity + "%";
+            todayWind.appendChild(todayHumid);
+        }
+    }
 }
 
-submitButton.addEventListener("click", getApi);
+submitButton.addEventListener("click", formSubmitHandler);
